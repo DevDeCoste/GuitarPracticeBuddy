0ed1e26096a26e5f10113538c839e746c9b68bd9
Dev <DevDeCoste@users.noreply.github.com>
2019-04-29 17:51:05 -0400

    Initial commit

---
+++ src/main/java/com/guitar/GuitarPracticeBuddy/GuitarPracticeBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.guitar.GuitarPracticeBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarPracticeBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarPracticeBuddyApplication.class, args);
+	}
+
+}
